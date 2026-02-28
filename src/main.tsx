56130f48d377dcc270a4b40e5435063a9d701312
Do Tran Thai An <justapie06@gmail.com>
2026-02-28 13:45:25 +0700

    feat: setup authgear

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,28 @@ import { Routes } from "@generouted/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider";
+import authgear from "@authgear/web";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Provider>
-      <Routes />
-    </Provider>
-  </StrictMode>,
-);
+async function init(): Promise<void> {
+  try {
+    await authgear.configure({
+      endpoint: import.meta.env.VITE_AUTHGEAR_ENDPOINT,
+      clientID: import.meta.env.VITE_AUTHGEAR_CLIENT_ID,
+      sessionType: "refresh_token",
+    });
+  } finally {
+    createRoot(document.getElementById("root")!).render(
+      <StrictMode>
+        <Provider>
+          <Routes />
+        </Provider>
+      </StrictMode>,
+    );
+  }
+}
+
+try {
+  await init();
+} catch (error) {
+  console.error(error);
+}
