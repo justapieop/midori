0d0ffbf637d7d7e5e14516edf16589d08fd5389e
Do Tran Thai An <justapie06@gmail.com>
2026-02-28 13:30:54 +0700

    chore: applies generouted setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
+import { Routes } from "@generouted/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Routes />
   </StrictMode>,
 );
