79c74c9dd36966e921cc7145c531fe59e7461f75
Do Tran Thai An <justapie06@gmail.com>
2026-03-05 21:41:44 +0700

    chore: cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,4 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), generouted(), tsconfigPaths()],
-  server: {
-    allowedHosts: ["13df-2405-4803-fdca-6f20-8881-ca13-bef3-719a.ngrok-free.app"]
-  }
 });
